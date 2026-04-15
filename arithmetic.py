db1570837686ef52c39c30d70fe38748ff45d5ff
Nilesh Ghule <nilesh@sunbeaminfo.com>
2026-04-15 17:00:54 +0530

    sqr fn

--- arithmetic.py
+++ arithmetic.py
@@ -11,8 +11,13 @@ def multiply(a, b):
 def divide(a, b):
     return a / b
 
+def square(a):
+    return a * a
+
 if __name__ == '__main__':
     print("add() - ", add(1, 2))
     print("subtract() - ", subtract(1, 2))
     print("multiply() - ", multiply(1, 2))
     print("divide() - ", divide(1, 2))
+    print("square() - ", square(1))
+
