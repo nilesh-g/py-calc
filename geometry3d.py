5a98535719ea365fffdec2a31c4abc286ec319a0
Nilesh Ghule <nilesh@sunbeaminfo.com>
2026-04-15 16:47:19 +0530

    cube area

--- geometry3d.py
+++ geometry3d.py
@@ -2,6 +2,11 @@
 def cube_volume(len):
     return len * len * len
 
+def cube_surface_area(len):
+    return 6 * len * len
+
 if __name__ == '__main__':
     print("cube_volume() - ", cube_volume(5))
+    print("cube_surface_area() - ", cube_surface_area(5))
+    
 
