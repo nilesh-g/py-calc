df095fd6d9ca1b43e3f5051dd801ac50f5776965
Meet Patel <mspmeet.patel@gmail.com>
2026-04-15 17:03:45 +0530

    cube added

--- arithmetic.py
+++ arithmetic.py
@@ -11,8 +11,12 @@ def multiply(a, b):
 def divide(a, b):
     return a / b
 
+def cube(a):
+    return a ** 3
+
 if __name__ == '__main__':
     print("add() - ", add(1, 2))
     print("subtract() - ", subtract(1, 2))
     print("multiply() - ", multiply(1, 2))
     print("divide() - ", divide(1, 2))
+    print("cube() - ", cube(3))
