a9b1862675f5f935443f01e5cd997e92a0e274a3
Nilesh Ghule <nilesh@sunbeaminfo.com>
2026-04-15 16:37:42 +0530

    rect area peri

---
+++ geometry2d.py
@@ -0,0 +1,10 @@
+
+def rect_area(len, br):
+    return len * br
+
+def rect_perimeter(len, br):
+    return 2 * (len + br)
+
+if __name__ == '__main__':
+    print("rect_area() - ", rect_area(1, 2))
+    print("rect_perimeter() - ", rect_perimeter(1, 2))
