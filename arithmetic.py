93bf411533325a3d1b03576a42ed4f4f828ffda5
Nilesh Ghule <nilesh@sunbeaminfo.com>
2026-04-15 16:35:45 +0530

    multiply & divide

--- arithmetic.py
+++ arithmetic.py
@@ -5,6 +5,14 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
 if __name__ == '__main__':
     print("add() - ", add(1, 2))
     print("subtract() - ", subtract(1, 2))
+    print("multiply() - ", multiply(1, 2))
+    print("divide() - ", divide(1, 2))
